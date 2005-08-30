0176ccf333038526887cbabd4c9cdf686a07af17
micah <micah>
2005-08-30 20:45:55 +0000

    Now works with events.py!

--- pyweek/main.py
+++ pyweek/main.py
@@ -20,19 +20,11 @@ pygame.display.set_caption('Test')
 screen = pygame.display.get_surface()
 
 # activate the event_handler
+eventnet._pygame.register_events()
 handle = events.event_handler()
 handle.capture()
 
-# func to use event_handler()
-def Handler(events):
-    for event in events:
-        if event.type == KEYDOWN:
-            eventnet.driver.post('QUIT')
-        else:
-            print event
-
 #loops to keep checking for events
 while 1:
-    #combine all event-getters (for lack of a better term) and make a single
-    # list with all events to pass to event_handler
-    Handler(pygame.event.get())
+    for event in pygame.event.get():
+        eventnet.driver.post(event.type, **event.dict)
