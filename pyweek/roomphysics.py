f02dfbdd86eefc6a66960283926b98b64797c648
adam <adam>
2005-09-01 19:40:06 +0000

    *** empty log message ***

--- pyweek/roomphysics.py
+++ pyweek/roomphysics.py
@@ -2,23 +2,22 @@ import ode
 
 
 class RoomPhysics:
-    def __init__(self, room, hero, drag=1):
+    def __init__(self, room, drag=1):
         self.world = room.world
         self.space = room.space
         self.coefficient = drag
         self.friction = 5000
-        self.hero = hero
         
-    def atmosphere(self, chars):
-            """This simulates a frictional force caused by the atmosphere
-    on the level. It will control how quickly the characters will
-    slow down when no force is applied. chars should be a list of ode Bodies."""
-            for char in chars:
-                u, v, w = char.getLinearVel()
-                char.addForce((u*self.coefficient,0,0))
+    def atmosphere(self, chars=[]):
+        """This simulates a frictional force caused by the atmosphere
+on the level. It will control how quickly the characters will
+slow down when no force is applied. chars should be a list of ode Bodies."""
+        for char in chars:
+            u, v, w = char.getLinearVel()
+            char.addForce((u*self.coefficient,0,0))
                 
     # Collision callback
-    def onNearCollision(contactgroup, geom1, geom2):
+    def onNearCollision(self, contactgroup, geom1, geom2):
         """Callback function for the collide() method.
     
 This function checks if the given geoms do collide and
@@ -35,11 +34,11 @@ creates contact joints if they do.
             cJoint = ode.ContactJoint(self.world, contactgroup, contact)
             cJoint.attach(geom1.getBody(), geom2.getBody())
             
-    def step(self):
+    def step(self, chars=[]):
         contactgroup = ode.JointGroup()
         for i in range(2):
             # Apply the atmosphere
-            self.atmosphere([self.hero])
+            self.atmosphere()
             
             # Detect collisions and create contact joints
             self.space.collide(contactgroup, self.onNearCollision)
