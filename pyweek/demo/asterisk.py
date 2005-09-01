2417f9ca3588b32af1d246067efd66431572eb63
michal <michal>
2005-09-01 16:07:13 +0000

    made the blocks thicker

--- pyweek/demo/asterisk.py
+++ pyweek/demo/asterisk.py
@@ -16,11 +16,11 @@ def makeRotatedRect(theta):
         b = math.sin(theta)
         c = -math.sin(theta)
         d = math.cos(theta)
-        print '<rect style="fill:#ccff00" width="100" height="10" x="50" y="50"'
+        print '<rect style="fill:#ccff00" width="100" height="25" x="0" y="0"'
         print 'transform="matrix(%s,%s,%s,%s,0,0)"/>' % (a,b,c,d)
 
 # print one without a rotation
-print '<rect style="fill:#993366" width="100" height="10" x="50" y="50"/>'
+#print '<rect style="fill:#993366" width="100" height="10" x="50" y="50"/>'
 
 
 # goal is to go around in a circle, using radians
