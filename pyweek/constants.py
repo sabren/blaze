1c4416a3e302ebb9b79c8cde62eb2b0d8d96bdd0
michal <michal>
2005-09-03 05:16:16 +0000

    added a bunch of constants

--- pyweek/constants.py
+++ pyweek/constants.py
@@ -2,3 +2,24 @@
 """
 LEFT = -1
 RIGHT = 1
+
+SPRITE_SIZE = (32,32)
+
+class SCREEN:
+    WIDTH=640
+    HEIGHT=480
+    
+
+class PHYSICS:
+    GRAVITY= (0,9.81,0)
+    ERP=0.8      # error reducing thingy in ode
+    CFM=10E-5    # some other thing from ode
+
+class HERO:
+    RADIUS = 16 # 32x32
+
+class ROOM:
+    THICKNESS = 32 # arbitrary depth for all our blocks
+    WIDTH = 540
+    HEIGHT = 480
+
