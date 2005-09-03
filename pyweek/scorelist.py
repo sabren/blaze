7d88dabb9fb68a2f5c18d5bc7d436a3275e6018e
micah <micah>
2005-09-03 22:22:49 +0000

    modified to work with highscores mode

--- pyweek/scorelist.py
+++ pyweek/scorelist.py
@@ -12,29 +12,46 @@ import unittest
 A great score list!
 """
 class ScoreList:
-    def __init__(self, scores=[], maxScores=10):
+    def __init__(self, names=[], scores=[], maxScores=10):
         self.scores = scores
+        self.names = names
         self.maxScores = maxScores
 
-        self.reorder()
+        #self.reorder()
+
+    def getScores(self):
+        # list = self.names
+        #num = 0
+        #for name in list:
+        #    list.insert(num, self.scores[num])
+        #    num = num / 2
+            
+        return zip(self.names, self.scores)
 
     def giveScore(self, score):
         """
         Add a score if it's good enough
         """
-        if len(self.scores) < self.maxScores:
-            self.scores.append (score)
-            self.reorder()
+        name = score[0]
+        score = score[1]
+        num = 0
+        while score < num:
+            num = num + 1
+        if not num:
+            self.scores.reverse()
+            self.names.reverse()
+            self.scores.append(score)
+            self.names.append(name)
+            self.scores.reverse()
+            self.names.reverse()                    
         else:
-            closest = None
-            
-            for s in self.scores:
-                if closest == None or s-score < closest:
-                    closest = s-score
-
-            self.scores.pop (self.scores.index (closest+score))
-            self.scores.append (score)
-            self.reorder()
+            self.scores.insert(num+1, score)
+            self.names.insert(num+1, name)
+        #no longer needed here
+        # self.scores.append (score)
+        # self.reorder()
+        if len(self.scores) > self.maxScores:
+            self.scores.pop ()
 
     def reorder(self):
         """
@@ -52,13 +69,14 @@ class ScoreListTest(unittest.TestCase):
         pass
 
     def test(self):
-        scoreList = ScoreList ([1, 2, 3, 4, 5], 3)
+        scoreList = ScoreList (['AAA', 'BBB', 'CCC', 'DDD', 'EEE'], [1, 2, 3, 4, 5])
         
-        scoreList.giveScore (6)
-        scoreList.giveScore (4)
-        scoreList.giveScore (5)
+        scoreList.giveScore (['FFF', 6])
+        scoreList.giveScore (['GGG', 7])
+        scoreList.giveScore (['HHH', 8])
+        print scoreList.getScores()
 
-        assert scoreList.scores == [6, 5, 5, 4, 4], "Le wtf, sir."
+#        assert scoreList.scores == [6, 5, 5, 4, 4], "Le wtf, sir."
 
         
 if __name__=="__main__":
