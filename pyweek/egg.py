e3f6e3921248ba825e161e3a8c4968269ae6409c
eykd <eykd>
2005-09-03 22:22:43 +0000

    Fixed the egg's .code

--- pyweek/egg.py
+++ pyweek/egg.py
@@ -3,4 +3,4 @@ from constants import CODE # Michal's UgLy hack. :)
 class Egg:
     def __init__(self):
         self.geom = ode.GeomSphere(space=None, radius=HERO.RADIUS)
-        self.geom.code = CODE.FOOD
+        self.geom.code = CODE.EGG
