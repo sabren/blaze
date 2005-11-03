30a3c806be0c024dd4a3d6afd2959a41085b33fa
micah <micah>
2005-11-03 19:30:12 +0000

    Ugly fix of my old py2exe system.

--- pyweek/tools/py2exe-python2.4.py
+++ pyweek/tools/py2exe-python2.4.py
@@ -1,68 +1,32 @@
-#code to convert python scripts into windows .exe files using py2exe
-#requires py2exe 0.5
+import os, glob
 
-#import modules or warn user if not possible
-try:
-    import os, string, time
-except:
-    raw_input('ERROR: Could not import needed modules, check your python lib')
-    pass
+# Ugly fix of my py2exe system.
 
-try: #system to avoid shutdown because of errors
+# constants
+old = 'c:\\\\cvs\\\\pyweek\main.py'
+old_dir = os.path.split(old)[0]
+setup_script = os.path.join(old_dir, 'setup.py')
+new = 'c:\\test'
+console = True
+data_files = glob.glob(os.path.join(old_dir, '*.png'))
+bundle = True
+icon = None
+xtra_mods = ['pygame.locals', 'pygame']
+Xcludes = ['w9xpopen', 'msvcr71']
+setup_code = '''# setup.py
+from distutils.core import setup
+import py2exe
 
-    #get the users temp file from the os module
-    temp = os.environ['TEMP']+'setup.py'
+setup(console=["%s"], data_files=[('', %s)], zipfile=None,
+            options = {"py2exe": { "dll_excludes": ["msvcr71.dll", "w9xpopen.exe"]}})
+''' % (old, data_files)
 
-    # take needed inputs from user and test for existing files
-    while 1:
-        src = raw_input('SOURCE:')
-        if os.path.exists(src):
-            break
-        else:
-            print "File not found, please try again."
-    while 1:
-        data = raw_input('DATA-DIR:')
-        if os.path.exists(data):
-            break
-        else:
-            print "File not found, please try again."
+cmd = '%s py2exe -c --dist-dir %s -b 1 %s -e w9xpopen.exe' % (setup_script, new,
+            ' '.join([' -i %s' % x for x in xtra_mods]))
 
-    new = '' #change this is you have a specific place you keep your converted files
-    if new == '':
-        new = os.environ['HOMEPATH']+'\\EXEs'
-        
-    #tell if this is a windows or console program (the windows option can also be used to make a program invisible)
-    mode = raw_input('Is this a console program?:')
-    if string.lower(mode) <> 'yes':
-        mode = 'windows'
-    else:
-        mode = 'console'
-
-    #make the text for a temporary setup.py script
-    text = '#setup.py \nfrom distutils.core import setup \nimport py2exe\nsetup('+mode+'="'+old+'")'
-
-    #create a command line with all options
-    cmd = temp, 'py2exe', mode, '--dist-dir', new
-
-    #make the setup script
-    file = open(temp, 'w')
-    file.write(text)
-    file.close()
-
-    #and run the command line
-    os.system(cmd)
-
-    #wait a couple of seconds while the command line is running
-    #I'm not sure if this is needed but it's here just in case
-    time.sleep(2)
-
-    #copy data files to the dist-dir
-    if data <> '':
-        shutil.copytree(data, new+os.path.split(data)[1])
-
-    #delete the setup script (finishing the program and closing)
-    os.remove(temp)
-
-except:
-    raw_input('ERROR!\nPossible Cause: missing or currupted python lib components')
-    pass
+# write script and run command line
+#'''
+open(setup_script, 'w').write(setup_code)
+#print cmd
+os.system(cmd)
+raw_input() #'''
