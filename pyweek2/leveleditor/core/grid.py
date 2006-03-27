33a00987111a6f10b10739123abaa8f8180b763a
Yango <Yango>
2006-03-27 05:35:39 +0000

    Better debugging prints

--- pyweek2/leveleditor/core/grid.py
+++ pyweek2/leveleditor/core/grid.py
@@ -10,7 +10,6 @@ class Grid:
         self.rows =  self.height/self.display_tile_size
         self.columns = self.width/self.display_tile_size
         self.matrix = map(list,self.rows*[self.columns*[None]])
-        print self.matrix
         self.init_grid()
 
     def init_grid(self):
@@ -21,9 +20,7 @@ class Grid:
             pos_hor += self.display_tile_size
             self.screen.fill((0,0,0),r) 
         pos_hor = 140
-        
         final_width = self.rows*self.display_tile_size
-        
         for i in xrange(self.columns+1):
             r = pygame.Rect(pos_hor,pos_ver,final_width,1)
             pos_ver += self.display_tile_size
@@ -31,13 +28,14 @@ class Grid:
         pygame.display.flip()
     
     def add_object_in_pos(self,object,pos):
-        if object is None: return
+        if object is None: 
+            print "You haven't selected a thing!"
+            return
         x,y = self.get_matrix_indexes(pos)
-        #print "TRYING (%s,%s)" % (x,y,)
         try:
             self.matrix[x][y] = object.name #Or a hash?
         except IndexError:
-            print "WRONG POSITION!!!!"
+            print "You have clicked outside of the grid!"
         self.print_non_nones()
     def get_matrix_indexes(self,pos):
         cx = pos[0] - 140
@@ -47,6 +45,7 @@ class Grid:
         cy = cy/self.display_tile_size
         return cx,cy
     def print_non_nones(self):
+        print "------------------------------------------------"
         for i in xrange(self.rows):
             for j in xrange(self.columns):
                 if not self.matrix[i][j] is None:
