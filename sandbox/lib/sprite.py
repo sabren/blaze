06401798e8374f11fadf171ac7fad3097beec6a1
micah <micah>
2007-01-29 22:53:56 +0000

    working pixel-perfect collision detection and demo

--- sandbox/lib/sprite.py
+++ sandbox/lib/sprite.py
@@ -24,12 +24,13 @@ class Sprite(pygame.sprite.Sprite,eventnet.driver.Handler):
         If using pixel perfect (usePP) you need to have a colorkey set.
         '''
 
+        self.points = []
         if self.usePP:
             if self.image.get_colorkey():
                 for y in range(self.image.get_height()):
                     for x in range(self.image.get_width()):
-                        if self.image.get_at((x,y)) != self.image.get_colorkey():
-                            point = (self.rect.left+x, self.rect.top+y)
+                        if self.image.get_at((x,y))[3] > 0:
+                            point = (self.rect.x+x, self.rect.y+y)
                             self.points.append(point)
             else:
                 raise AttributeError, 'Sprite.image needs a colorkey for this operation!'
@@ -76,7 +77,29 @@ class AnimatedSprite(Sprite):
 
 if __name__=='__main__':
     import engine,state
-    s = State()
-    def tick():
-        pygame.display.get_surface().fill((0,0,0))
+
     Sprite.image = pygame.image.load('howitzer.png')
+    Sprite.image.set_colorkey((255,255,255))
+    Sprite.usePP = True
+    sprite = Sprite()
+    sprite.rect.topleft = (50,50)
+
+    s = state.State()
+    def tick():
+        disp = pygame.display.get_surface()
+        disp.fill((0,0,0))
+        disp.blit(sprite.image,sprite.rect)
+        pygame.display.flip()
+
+    def motion(event):
+        if sprite.collidePoint(event.pos):
+            print 'COLLISION!!!!'
+
+    s.EVT_MouseMotion = motion
+
+    s.tick = tick
+    #s = s()
+    e = engine.Engine()
+    e.DEFAULT = s
+    e.size = (300,200)
+    e.run()
