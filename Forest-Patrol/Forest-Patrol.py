fd243bc6c78bddc578d115bf3b7eafe056517b8c
micah <micah>
2007-03-20 10:33:43 +0000

    Created lib dir according to richard's skellington demo

--- Forest-Patrol/Forest-Patrol.py
+++ Forest-Patrol/Forest-Patrol.py
@@ -16,7 +16,8 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 from directicus.engine import Engine
-import menus,pygame
+from lib import menus
+import pygame
 
 class Game(Engine):
 
