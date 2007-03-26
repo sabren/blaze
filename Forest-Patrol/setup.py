0175a49ffcfd5600c9c0c0da1b09a71cc3f8f3e3
micah <micah>
2007-03-26 18:32:34 +0000

    Fixed?

--- Forest-Patrol/setup.py
+++ Forest-Patrol/setup.py
@@ -1,21 +1,24 @@
 from distutils.core import setup
 import glob
 
-# If we can't import py2exe we just won't use it
+extra_opts = dict(zipfile=None)
+
+# If py2exe exists we'll import it
 try:
     import py2exe
+    extra_opts.update({'windows': ['Forest-Patrol.py']})
 except ImportError:
     pass
 
 # Same for py2app
 try:
     import py2app
+    extra_opts.update({'zipfile': '', # zipfile=None confuses py2app
+                       'app': ['Forest-Patrol.py']})
 except ImportError:
     pass
 
-setup(windows=['Forest-Patrol.py'],
-      app=['Forest-Patrol.pyw'],
-      data_files=[
+setup(data_files=[
           ('',['README.txt','LICENSE.txt']),
           ('data',glob.glob('data/*.png')+['data/font.ttf']),
           ('data/music',glob.glob('data/music/*.mp3')+glob.glob('data/music/*.wav')),
@@ -33,5 +36,5 @@ setup(windows=['Forest-Patrol.py'],
       keywords='pygame,RTS',
       install_requires=['pygame>=1.7.1','directicus>=0.1'],
       download_url='http://blazeofglory.org/projects/fp',
-      zipfile=None,
+      **extra_opts
       )
